6ede3fef00eeff31f05fd390766304b0276ff2c4
Lyuba Bozhilova <lyubabozhilova@gmail.com>
2022-11-08 11:55:44 +0000

    update shift analysis

--- 03-shift-analysis.R
+++ 03-shift-analysis.R
@@ -2,7 +2,7 @@
 ##### Distribution analysis #####
 #################################
 
-# Last edited: 20/07/22 by LVB
+# Last edited: 08/11/22 by LVB
 
 # Description: Do autophagy gene knockouts have an effect on mother-to-pup
 # heteroplasmy shift?
@@ -18,14 +18,15 @@ load("data/parsed/01-parsed-data.RData")
 
 #----- Compare Bcl2l13 del/del controls to the remaining groups
 shifts <- split(df$Shift, df$Genotype_mother)
+shifts$Ulk1_noNNT <- filter(df, Genotype_mother == "Ulk1:del/del" & !is_NNT)$Shift
 
-bcl_pvals <- gt_nms[-2] %>% 
+bcl_pvals <- names(shifts)[-2] %>% 
   sapply(function(gt) ks.test(shifts[["Bcl2l13:wt/wt"]], shifts[[gt]])$p.value) %>% 
   round(3)
 bcl_pvals
 
 # Bonferroni correction for significance
-(bcl_pvals < .05 / 5) %>% which %>% names # only Bcl2l13:del/del
+(bcl_pvals < .05 / 5) %>% which %>% names # Bcl2l13:del/del & Ulk1:del/del
 
 #----- Do Wilcoxon test for shift being zero
 w.zero.pvals <- shifts %>% sapply(function(x) wilcox.test(x)$p.value) 
@@ -41,15 +42,15 @@ ctr_shift <- ctr_mother_ids %>%
 
 # Test each of the genotype groups against each of the 210 pseudo-controls
 test_shift <- function(gt){
-  shift <- filter(df, Genotype_mother == gt)$Shift
+  shift <- shifts[[gt]]
   data.frame(Genotype_mother = gt,
              ctr_idx = 1:210,
              pval = sapply(ctr_shift, function(ctr) ks.test(ctr, shift)$p.value))
 }
-test_df <- gt_nms %>% 
+test_df <- names(shifts) %>% 
   lapply(test_shift) %>% 
   bind_rows()
-test_df$Genotype_mother <- factor(test_df$Genotype_mother, levels = gt_nms)
+test_df$Genotype_mother <- factor(test_df$Genotype_mother, levels = c(gt_nms, "Ulk1_noNNT"))
 
 # Bonferroni correction for bootstrap tests
 psig <- .05 / 6
@@ -64,7 +65,7 @@ median_pvals <- split(test_df$pval, test_df$Genotype_mother) %>%
   sapply(median) %>% 
   round(3)
 median_pvals
-(median_pvals < psig) %>% which %>% names # "Bcl2l13:del/del" "Ulk1:del/del" 
+(median_pvals < psig) %>% which %>% names # "Bcl2l13:del/del" "Ulk1:del/del" "Ulk1_noNNT" 
 
 # Discretise p-values to < 0.01, < 0.05
 test_df$p_gr <- case_when(test_df$pval < 0.01 ~ "p < 0.01", 
